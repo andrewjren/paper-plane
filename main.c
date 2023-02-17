652f0f4026514546aaa134b7c5b4ebc94f2543d9
Andrew Ren <andrewjren@gmail.com>
2023-02-17 08:07:49 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "EPD_Test.h"
+
+int main() {
+    stdio_init_all();
+    //setup_default_uart();
+    //printf("Hello, world!\n");
+    DEV_Delay_ms(500);
+
+    EPD_7in5_V2_test();
+
+    return 0;
+}
